b544e3e1c4370d7df3738835dffc21228b81c9a5
entrega0 <lfic1997@gmail.com>
2023-08-20 12:51:58 -0300

    actualizando-entrega1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
+    let recordado=false;
+    let usuarioguardado=localStorage.getItem('usuario');
+    let passwordguardada=localStorage.getItem('password');
+    if(usuarioguardado!=null && passwordguardada!=null){
+        recordado=true;
+    }
+    if(!recordado){
+        if(sessionStorage.getItem('usuario')==null && sessionStorage.getItem('password')==null){
+            window.location = "login.html";
+        }
+    }
+   
+    document.getElementById('close-session').addEventListener("click", function(){
+        localStorage.removeItem('usuario');
+        localStorage.removeItem('password');
+    });
+
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
---
+++ js/products.js
@@ -0,0 +1,55 @@
+const URL="https://japceibal.github.io/emercado-api/cats_products/101.json";
+let productos = [];
+
+async function getData(){
+    let response = await fetch(URL);
+    let data = await response.json();
+    let array = await data.products;
+    return array;
+}
+
+async function showProducts(){
+    const container = document.getElementById('contenedor-productos');
+    productos = await getData();
+    for(let i=0; i<productos.length; i++){
+        container.innerHTML+=`
+        <div class="list-group-item">
+            <div class="row">
+                <div class="col-3">
+                    <img class="img-thumbnail" src="${productos[i].image}">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">${productos[i].name} - ${productos[i].currency} ${productos[i].cost}</h4>
+                        <p class="ventas">${productos[i].soldCount} ventas</p>
+                    </div>
+                    <p class="mb-1">${productos[i].description}</p>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+showProducts();
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    //chequear login
+    let recordado=false;
+    let usuarioguardado=localStorage.getItem('usuario');
+    let passwordguardada=localStorage.getItem('password');
+    if(usuarioguardado!=null && passwordguardada!=null){
+        recordado=true;
+    }
+    if(!recordado){
+        if(sessionStorage.getItem('usuario')==null && sessionStorage.getItem('password')==null){
+            window.location = "login.html";
+        }
+    }
+   
+    document.getElementById('close-session').addEventListener("click", function(){
+        localStorage.removeItem('usuario');
+        localStorage.removeItem('password');
+    });
+    
+});
