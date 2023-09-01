9231218ac9423e99c75d6bfad17c536b8d72c865
Martín Capuano <capuano.martin@outlook.com>
2023-09-01 12:32:10 -0300

    Buscador sin funcion

--- js/products.js
+++ js/products.js
@@ -1,6 +1,6 @@
 let categoria = localStorage.getItem('catID');
 const URL=`https://japceibal.github.io/emercado-api/cats_products/${categoria}.json`;
-let lista = [];
+let listaOrigen = [];
 
 async function getData(){
     let result = [];
@@ -15,8 +15,8 @@ async function getData(){
 }
 async function dataList(){
     try{
-        lista = await getData();
-        showProducts(lista);
+        listaOrigen = await getData();
+        showProducts(listaOrigen);
     }catch(error){
         console.log(error);
     }
@@ -70,22 +70,22 @@ document.addEventListener('DOMContentLoaded',function(){
 
 //ordenanza de articulos----------------------------
 document.getElementById('sort$Asc').addEventListener('click', function(){
-    let array = lista;
+    let array = listaOrigen;
     array.sort((a,b) => a.cost>b.cost);
     showProducts(array);
 });
 document.getElementById('sort$Desc').addEventListener('click', function(){
-    let array = lista;
+    let array = listaOrigen;
     array.sort((a,b) => a.cost<b.cost);
     showProducts(array);
 });
 document.getElementById('sortByRel').addEventListener('click', function(){
-    let array = lista;
+    let array = listaOrigen;
     array.sort((a,b) => a.soldCount<b.soldCount);
     showProducts(array);
 });
 document.getElementById('rangeFilterCount').addEventListener('click', function(){
-    let array = lista;
+    let array = listaOrigen;
     let min = document.getElementById('rangeFilterCountMin');
     let max = document.getElementById('rangeFilterCountMax');
     if(min.value == 0 && max.value != 0){
@@ -95,7 +95,7 @@ document.getElementById('rangeFilterCount').addEventListener('click', function()
         showProducts(array.filter((element) => element.cost>min.value));
     }
     else if(min.value == 0 && max.value == 0){
-        showProducts(lista);
+        showProducts(listaOrigen);
     }
     else{
         showProducts(array.filter((element) => element.cost>min.value && element.cost<max.value));
@@ -104,7 +104,16 @@ document.getElementById('rangeFilterCount').addEventListener('click', function()
 document.getElementById('clearRangeFilter').addEventListener('click', function(){
     document.getElementById('rangeFilterCountMin').value = null;
     document.getElementById('rangeFilterCountMax').value = null;
-    showProducts(lista);
+    showProducts(listaOrigen);
+});
+//--------------------------------------------------
+
+//buscador------------------------------------------
+const buscar = document.getElementById('search');
+buscar.addEventListener('input', function(){
+    let array = listaOrigen;
+    
+    showProducts(array);
 });
 //--------------------------------------------------
 
