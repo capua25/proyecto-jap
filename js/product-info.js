b61e63f55a9330eb3af9baf625c3ec53b0cb9873
Martín Capuano <capuano.martin@outlook.com>
2023-09-14 21:50:29 -0300

    Entrega 3 funcionando

--- js/product-info.js
+++ js/product-info.js
@@ -48,20 +48,53 @@ function showInfo(articulo){
     }
 }
 
-
  function showComments(commentsList){
      const comentarios = document.getElementById("comentarios");
     comentarios.innerHTML = '';
+    console.log(commentsList)
     commentsList.forEach((comentario) => {
+        let arr = [];
+        for(let i=0;i<5;i++){
+            if(i<comentario.score){
+                arr.push(`<span class="fa fa-star checked"></span>`);
+            }else{
+                arr.push(`<span class="fa fa-star"></span>`);
+            }
+        };
         comentarios.innerHTML += `
         <div>
-        <p class="h3">Comentarios</p>
-        <p>${comentario.user} ${comentario.dateTime}</p>
-        <p>${comentario.description}</p>
-        </div>`
-    })
-    console.log(comentarios)
+            <p><strong>${comentario.user}</strong> - ${comentario.dateTime} - ${arr[0]}${arr[1]}${arr[2]}${arr[3]}${arr[4]}
+            </p>
+            <p>${comentario.description}</p>
+        </div>`;
+    });
 };
+
+function pushComment(){
+    const puntuacion = document.getElementById('puntos');
+    const comentario = document.getElementById('comentario');
+    if(comentario.value.trim()==''){
+        alert("Comentario vacío");
+        return 0;
+    }
+    let puntos = puntuacion.selectedOptions[0].value;
+    const usuario = sessionStorage.getItem('usuario');
+    const date = new Date();
+    let actual = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDay()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+
+    let commentObj = {
+        product: ProdID,
+        score: puntos,
+        description: comentario.value,
+        user: usuario,
+        dateTime: actual,
+    };
+    //Aca iria el fetch con el metodo POST
+
+    comentario.value = '';
+    arrComments.push(commentObj);
+    showComments(arrComments);
+}
 
 getData();
 getComments();
