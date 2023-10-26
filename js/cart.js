d6e251414328c8b3a440838e300861b680c8df1b
Martín Capuano <capuano.martin@outlook.com>
2023-10-26 16:09:31 -0300

    Arreglado subtotales carrito

--- js/cart.js
+++ js/cart.js
@@ -48,15 +48,23 @@ function showInfo(array) {
         const container = document.getElementById("carritoCompras");
         container.innerHTML = '';
         array.forEach((element) => {
+            let value = 0;
+            if (element.currency == 'USD') {
+                value = element.unitCost;
+            }
+            else if (element.currency == 'UYU') {
+                value = Math.round(element.unitCost / 40);
+            }
             container.innerHTML += `
                 <tr class="table-primary">
                     <th scope="row"><img src="${element.image}" height="100px" alt=""></th>
                     <td class="text-center">${element.name}</td>
                     <td class="text-center price">${element.currency}${element.unitCost}</td>
                     <td ><input class="subtotal" type='number' value='${element.count}' min='1' max='99'></td>
-                    <td class="fw-bold text-center" >${element.currency}${element.unitCost}</td>
-                    <td class="text-center" width="50px"><button type="button" class="btn" onclick="removeFromCart(${element.id})"><svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" fill="red"></path> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" fill="red"></path> </svg></button></td>
-                </tr>`
+                    <td class="fw-bold text-center" >USD${value}</td>
+                    <td class="text-center" width="50px"><button type="button" class="btn" onclick="removeFromCart(${element.id})"><svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" fill="red"></path> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" fill="red"></path> </svg></button></td></tr>`
+
+            
         });
         const inputs = document.querySelectorAll('input.subtotal');
         const subt = document.querySelectorAll('td.fw-bold.text-center');
@@ -66,7 +74,12 @@ function showInfo(array) {
                 if (element.value < 0 || element.value > 99) {
                     element.value = 1;
                 }
-                subt[i].innerHTML = 'USD' + element.value * Number(prices[i].textContent.substring(3));
+                if (prices[i].textContent.substring(0, 3) == 'UYU') {
+                    subt[i].innerHTML = 'USD' + Math.round(element.value * (Number(prices[i].textContent.substring(3)/40)));
+                }
+                else if (prices[i].textContent.substring(0, 3) == 'USD') {
+                    subt[i].innerHTML = 'USD' + Math.round(element.value * Number(prices[i].textContent.substring(3)));
+                }
                 Subtotal();
             });
         });
